c2beff2e751d12d5cf12b053b15248c2413313b7
ZoneOfDevelopment <codezoneofdevelopment@gmail.com>
2021-12-31 13:12:10 +0000

    new project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+
+namespace ConsumerForRabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // definition of Connection 
+            var _rabbitMQServer = new ConnectionFactory() { HostName = "localhost", Password = "guest", UserName = "guest" };
+
+            // create connection
+            using var connection = _rabbitMQServer.CreateConnection();
+
+            // create channel
+            using var channel = connection.CreateModel();
+
+            // Read messages
+            ReadMessage.Start(channel, "testRabbitMQ");
+        }
+    }
+}
---
+++ ReadMessage.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsumerForRabbitMQ
+{
+    internal static class ReadMessage
+    {
+        public static void Start(IModel channel, string queueName)
+        {
+            // connect to the queue
+            channel.QueueDeclare(queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            // Consumer definition
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Definition of event when the Consumer gets a message
+            consumer.Received += (sender, e) => {
+                ManageMessage(e);
+            };
+
+            // Start pushing messages to our consumer
+            channel.BasicConsume(queueName, true, consumer);
+
+            Console.WriteLine("Consumer is running");
+            Console.ReadLine();
+        }
+
+        private static void ManageMessage(BasicDeliverEventArgs e)
+        {
+            // Obviously for this demo, we just print the message...
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        }
+    }
+}
